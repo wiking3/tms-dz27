cc8f4bd6cbc5ec507762dc54e8f4746a5469f556
Egor <70162439+wiking3@users.noreply.github.com>
2026-01-06 23:54:32 +0300

    multiply_numbers

---
+++ ex1.py
@@ -0,0 +1,8 @@
+#написать функцию multiply_numbers() которая принимает 2 параметра и возвращает их проивзедение и вызвать функцию.
+
+def multiply_numbers(a,b):
+    return a*b
+
+print(f"-> {multiply_numbers(1,7)}")
+print(f"-> {multiply_numbers(2,7)}")
+print(f"-> {multiply_numbers(3,7)}")
